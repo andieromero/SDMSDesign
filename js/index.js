13cde0376e19b720912aaab323c075be89161955
Andie Romero <andriana@beltranfamily.com>
2015-08-06 15:39:26 -0700

    testing slick

--- js/index.js
+++ js/index.js
@@ -15,8 +15,6 @@ jQuery(document).ready(function(){
 
 
 var width = $(window).width();
-console.log(width);
-
 if (width < (768)) {
 	$('.dropdown-toggle').prop('disabled', true);
 	$('.dropdown-toggle').attr("href", "schedule.html");
@@ -33,3 +31,43 @@ if (width < (768)) {
 
 // var text = $.getJSON("json/content.json")
 
+$(document).ready(function(){
+	$('.responsive').slick({
+
+	  dots: true,
+	  arrows: true,
+	  infinite: false,
+	  speed: 300,
+	  slidesToShow: 4,
+	  slidesToScroll: 4,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	        infinite: true,
+	        arrows: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]
+	});
+});
