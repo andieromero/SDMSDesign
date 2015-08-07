93e632098c08b54cfe47afddc36cb9bdc0b357da
Andie Romero <andriana@beltranfamily.com>
2015-08-07 09:31:02 -0700

    pre pull

--- js/index.js
+++ js/index.js
@@ -35,6 +35,7 @@ $(document).ready(function(){
 	$('.responsive').slick({
 
 	  dots: true,
+	  arrows: true,
 	  infinite: false,
 	  speed: 300,
 	  slidesToShow: 4,
@@ -46,21 +47,24 @@ $(document).ready(function(){
 	        slidesToShow: 3,
 	        slidesToScroll: 3,
 	        infinite: true,
+	        arrows: true,
 	        dots: true
 	      }
 	    },
 	    {
-	      breakpoint: 600,
+	      breakpoint: 768,
 	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 2
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	        arrows: false
 	      }
 	    },
 	    {
 	      breakpoint: 480,
 	      settings: {
-	        slidesToShow: 1,
-	        slidesToScroll: 1
+	        slidesToShow: 2,
+	        slidesToScroll: 2,
+	        arrows: false
 	      }
 	    }
 	    // You can unslick at a given breakpoint now by adding:
@@ -69,3 +73,39 @@ $(document).ready(function(){
 	  ]
 	});
 });
+
+    $(function () {
+        // Remove Search if user Resets Form or hits Escape!
+		$('body, .navbar-collapse form[role="search"] button[type="reset"]').on('click keyup', function(event) {
+			console.log(event.currentTarget);
+			if (event.which == 27 && $('.navbar-collapse form[role="search"]').hasClass('active') ||
+				$(event.currentTarget).attr('type') == 'reset') {
+				closeSearch();
+			}
+		});
+
+		function closeSearch() {
+            var $form = $('.navbar-collapse form[role="search"].active')
+    		$form.find('input').val('');
+			$form.removeClass('active');
+		}
+
+		// Show Search if form is not active // event.preventDefault() is important, this prevents the form from submitting
+		$(document).on('click', '.navbar-collapse form[role="search"]:not(.active) button[type="submit"]', function(event) {
+			event.preventDefault();
+			var $form = $(this).closest('form'),
+				$input = $form.find('input');
+			$form.addClass('active');
+			$input.focus();
+
+		});
+		// ONLY FOR DEMO // Please use $('form').submit(function(event)) to track from submission
+		// if your form is ajax remember to call `closeSearch()` to close the search container
+		$(document).on('click', '.navbar-collapse form[role="search"].active button[type="submit"]', function(event) {
+			event.preventDefault();
+			var $form = $(this).closest('form'),
+				$input = $form.find('input');
+			$('#showSearchTerm').text($input.val());
+            closeSearch()
+		});
+    });
